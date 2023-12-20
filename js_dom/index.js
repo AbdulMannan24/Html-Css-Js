2ebd90da6e9f053faf9e1deafc15faf6da6c51f4
Mohd Abdul Mannan <ma24042000@gmail.com>
2023-12-20 14:18:24 +0530

    js dom'

---
+++ js_dom/index.js
@@ -0,0 +1,8 @@
+//DOM manipulation 
+
+//getElementById("id")
+// let element = document.getElementById("first");
+//getElementByClassName
+
+let first = document.getElementById("first");
+first.innerText = "learn js dom very efficiently"
